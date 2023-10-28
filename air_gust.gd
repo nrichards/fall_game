31e242092a2fae8f4c20803a4ce8e5466354ed07
Nick Richards <nrichards@gmail.com>
2023-10-28 10:49:29 -0700

    Strip stubs (#2)
    * Strip stubs
    
    Contributes to #1
    
    * Juiced, added fun
    * Responsive on touch
    * Blower vibrates
    * Smoother sounds

--- air_gust.gd
+++ air_gust.gd
@@ -1,21 +1,28 @@
 extends Sprite2D
 
 @onready var area_2d = $Area2D
-@onready var collision_shape_2d = $Area2D/CollisionShape2D
+@onready var leaf_blower = $"../.."
 
 var original_gse
 
 func _ready():
+	leaf_blower.moving.connect(is_moving)
+
 	original_gse = area_2d.gravity_space_override
 	area_2d.gravity_space_override = area_2d.SPACE_OVERRIDE_DISABLED
+	
 
 
 func is_moving(moving):
-	if moving and not visible:
-		visible = true
-		area_2d.gravity_space_override = original_gse
+	if moving: 
+		if not visible:
+			visible = true
+
+			area_2d.gravity_space_override = original_gse
 		
-	elif not moving and visible:
-		visible = false
-		area_2d.gravity_space_override = area_2d.SPACE_OVERRIDE_DISABLED
+	else:
+		if visible:
+			visible = false
+
+			area_2d.gravity_space_override = area_2d.SPACE_OVERRIDE_DISABLED
 
--- player.gd
+++ player.gd
@@ -1,37 +1,34 @@
 extends CharacterBody2D
 
 @export var speed = 500
-
-@onready var air_gust = $LeafBlower/AirGust
 @onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
+signal moving(is_moving)
+
+const stopped_distance = 15
+
 var click_position
+
 
 func _ready():
 	click_position = Vector2(position.x, position.y)
 	
 	
-	
 func _physics_process(delta):
-	if Input.is_action_pressed("left_click"):
+	var pressed = Input.is_action_pressed("left_click")
+	
+	if pressed:
 		click_position = get_global_mouse_position()
 		
 	var target_position = (click_position - position).normalized()
 	
-	if position.distance_to(click_position) > 15:
+	if pressed or position.distance_to(click_position) > stopped_distance:
 		velocity = target_position * speed
 		move_and_slide()
-		
-		air_gust.is_moving(true)
+		moving.emit(true)
 	
-		if not audio_stream_player_2d.playing:
-			audio_stream_player_2d.play()
-	else:
-		air_gust.is_moving(false)
-
-		if audio_stream_player_2d.playing:
-			audio_stream_player_2d.stop()
+	elif not pressed:
+		moving.emit(false)
 
 
-func listen_to_physics(body):
-	print("physics did something " , body)
+
